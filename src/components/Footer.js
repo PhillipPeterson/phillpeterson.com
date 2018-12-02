3563503d60e9e643d431ccec86baa54291a4164d
Phillip <majeh90210@gmail.com>
2018-12-02 15:34:00 -0700

    some more changes, added contact info

---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { MdMail } from 'react-icons/md';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { Button, Row, Col } from 'reactstrap';
+
+
+class Footer extends React.Component {
+
+    onContactButtonClick(link) {
+        if (link === 'email') {
+            window.open('mailto:majeh90210@gmail.com?Subject=Response%20From%20Portfolio%20Site');
+        } else if (link === 'linkedin') {
+            window.open('https://www.linkedin.com/in/phillippeterson');
+        } else if (link === 'git') {
+            window.open('https://www.github.com/phillippeterson');
+        }
+    }
+
+    render() {
+        return (
+            <Row>
+                    <span className="d-block pb-4 h2 text-dark border-bottom border-gray">Contact</span>
+                    <p><b>Email</b>: phillip1.peterson@umontana.edu | <b>Phone</b>: (406)-890-9262</p>
+                    <div>
+                        <Button onClick={() => this.onContactButtonClick('email')}>
+                            <MdMail />
+                        </Button>
+                        <Button onClick={() => this.onContactButtonClick('linkedin')}>
+                            <FaLinkedin />
+                        </Button>
+                        <Button onClick={() => this.onContactButtonClick('git')}>
+                            <FaGithub />
+                        </Button>
+                    </div>
+            </Row>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/components/resusable.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+export class Reusable {
+
+    static WhiteContainer = styled.div`
+    background-color: ##fdfdfd;
+    color: rgba(0, 0, 0, 0.87);
+    padding: 0 32px 32px 32px;
+    height: inherit;
+    text-align: left;`;
+
+    static ContainerDivider = styled.hr`
+    margin: 0 0 32px 0;
+    border-top: solid 1px rgba(0,0,0,.1)`;
+
+    static SectionHeader = styled.h2`
+    font-weight: bold;
+    margin-bottom: 32px;
+    color: rgba(0, 0, 0, 0.87);`;
+
+    static ExternalLink = styled.a`
+    color: rgba(0, 0, 0, 0.87);
+    text-decoration: underline;
+    font-weight: bold;
+    text-align: center;`;
+
+    static ListItem = styled.h5`
+    font-weight: bold;
+    margin: 16px 0 4px 0;
+    color: rgba(0, 0, 0, 0.87);`;
+
+    static ItemText = styled.p`
+    line-height: 1.8;
+    margin-bottom: 16px;
+    margin-left: 2px;
+    color: rgba(0, 0, 0, 0.87);`;
+
+    static ButtonStyles = {
+        email: {
+            backgroundColor: '#ff4343',
+            color: 'white',
+            margin: '0 12px 0 0'
+        },
+        linkedin: {
+            backgroundColor: '#0077B5',
+            color: 'white',
+            margin: '0 12px 0 12px'
+        },
+        github: {
+            backgroundColor: 'whitesmoke',
+            color: 'black',
+            margin: '0 12px 0 12px'
+        }
+    }
+}
+
+export default Reusable;
