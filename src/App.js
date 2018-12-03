216e54c0d56508c4edf8868ea7fb2c6f389c5db8
Phillip <majeh90210@gmail.com>
2018-12-03 12:09:14 -0700

    more changes to layout, removed post and unnecessary card stuff, centered card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Fragment } from 'react';
-import axios from 'axios';
 import { Container, Row, Col } from 'reactstrap';
-
-import Post from './components/Post';
 import Header from './components/Header';
-import SideCard from './components/SideCard';
+import Card from './components/Card';
 import Footer from './components/Footer';
 
 const App = () => (
@@ -15,20 +12,9 @@ const App = () => (
     <main className="my-5 py-5">
       <Container className="px-0">
         <Row noGutters className="pt-2 pt-md-5 w-100 px-4 px-xl-0 position-relative">
-        
-          <Col xs={{ order: 2 }} md={{ size: 4, order: 1 }} tag="aside" className="pb-5 mb-5 pb-md-0 mb-md-0 mx-auto mx-md-0">
-            <SideCard />
-          </Col>
-          
-          <Col xs={{ order: 1 }} md={{ size: 7, offset: 1 }} tag="section" className="py-5 mb-5 py-md-0 mb-md-0">
-            <Post />
-          </Col>
-          
-        </Row>
-          
-        <Row>
           <Col xs={4} sm={4} md={4} />
-          <Col xs={4} sm={4} md={5} >
+          <Col xs={4} md={5} className="pb-5 mb-5 pb-md-0 mb-md-0 mx-auto mx-md-0">
+            <Card />
             <Footer />
           </Col>
           <Col xs={4} sm={4} md={4} />
---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react';
+
+import {
+  Button, UncontrolledAlert, Card, CardImg, CardBody,
+  CardTitle, CardSubtitle, CardText
+} from 'reactstrap';
+
+const BANNER = 'https://i.imgur.com/CaKdFMq.jpg';
+
+const PortfolioCard = () => (
+  <Fragment>
+    
+    <Card>
+      <CardBody>
+        <CardTitle className="h3 mb-2 pt-2 font-weight-bold text-secondary">Best Site Ever!</CardTitle>
+        <CardText className="text-secondary mb-4" style={{ fontSize: '0.75rem' }}>
+          Hello! Welcome to my personal website! As you can see there's not much here yet 
+          but it will eventually become a site for hosting my own portfolio and resume! 
+        </CardText>
+      </CardBody>
+    </Card>
+    
+  </Fragment>
+);
+
+export default PortfolioCard;
