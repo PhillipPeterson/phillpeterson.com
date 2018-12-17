2eac6105fa26bac104658fcdb74889f327189493
Phillip <majeh90210@gmail.com>
2018-12-16 17:03:05 -0700

    Card uses props

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,10 +11,9 @@ class PortfolioCard extends React.Component {
 
 				<Card style={{ marginBottom: 30 }}>
 					<CardBody>
-						<CardTitle className="h3 mb-2 pt-2 font-weight-bold text-secondary">Best Site Ever!</CardTitle>
+						<CardTitle className="h3 mb-2 pt-2 font-weight-bold text-secondary">{this.props.title}</CardTitle>
 						<CardText className="text-secondary mb-4" style={{ fontSize: '0.75rem' }}>
-							Hello! Welcome to my personal website! As you can see there's not much here yet
-							but it will eventually become a site for hosting my own portfolio and resume!
+							{this.props.text}
 					</CardText>
 					</CardBody>
 				</Card>
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -7,9 +7,9 @@ class Portfolio extends React.Component {
     render() {
         return (
             <Fragment>
-                <PortfolioCard />
-                <PortfolioCard />
-                <PortfolioCard />
+                <PortfolioCard title='Card 1' text='This is card 1' />
+                <PortfolioCard title='Card 2' text='This is card 2'/>
+                <PortfolioCard title='Card 3' text='This is card 3'/>
             </Fragment>
         );
     }
