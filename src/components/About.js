3670b7c6c29a65ca212fd003a6a71835c8a5b29d
Phillip <majeh90210@gmail.com>
2019-10-19 13:06:15 -0600

    Changed about a bit

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,10 @@ class About extends React.Component {
 
             <div>
                 <h2>About me</h2>
-                <p>I am a freshly graduated computer science student looking to make a name with your company.</p> 
+                <p>I am a freshly graduated computer science student looking to leave my mark on the world.</p> 
                 <p>I have mainly used Java but I have database experience and I can work on the front end as well!</p>
+                <p>I am currently on my way to becoming a certified Salesforce Administrator 
+                    and a dedicated member to the Salesforce community!</p>
 
             </div>
 
